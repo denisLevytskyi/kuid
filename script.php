5a248b7ec4a0fe45d6eccb6868df103e0a122bf3
Your Name <tesu@example.com>
2024-05-19 18:19:17 +0300

    PROD

---
+++ script.php
@@ -0,0 +1,30 @@
+<?php
+
+$step = $_POST['step'];
+$stop = $_POST['stop'];
+$kuid = explode($stop, $_POST['kuid']);
+$miss = 0;
+$kuid_list = array();
+$kuid_list_control = array();
+
+foreach ($kuid as $key => $value) {
+	if (in_array($value, $kuid_list_control) == false) {
+		array_push($kuid_list_control, $value);
+		array_push($kuid_list, $value);
+	} else {
+		$miss++;
+	};
+	if ( (count($kuid_list) == $step) || ($key == count($kuid) - 1) ) {
+		$kuid_str = implode(",", $kuid_list);
+		echo ("<xmp>" . ($kuid_str) . "</xmp>");
+		$kuid_list = array();
+	};
+};
+
+echo (
+	'<pre>' .
+		'ПОЛУЧЕНО KUIDs   : ' . count($kuid) . '<br>' .
+		'ИЗ НИХ ПОВТОРОВ  : ' . $miss . '<br>' . 
+		'ВОЗВРАЩЕНО KUIDs : ' . (count($kuid) - $miss) .
+	'</pre>'
+);
